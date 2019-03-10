53c0b9adc853e91527a0198dbf06a322913cfe3d
EJ Fox <ejfox@ejfox.com>
2019-03-10 19:39:17 -0400

    Set up tone.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" name="button"
+      class="pa5 ba bg-green white br3 ma3 center db pointer"
+      @click="start">
+    Start</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Tone from 'tone'
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  mounted: function () {
+    const synth = new Tone.Synth().toMaster()
+    // synth.triggerAttackRelease('E4', '4n')
+    const loop = new Tone.Loop(function (time) {
+      synth.triggerAttackRelease('E2', '4n')
+    }, '1n')
+
+    const loop2 = new Tone.Loop(function (time) {
+      synth.triggerAttackRelease('A2', '8n')
+    }, '4n')
+
+    // const loop3 = new Tone.Loop(function (time) {
+    //   synth.triggerAttackRelease('A2', '8n')
+    // }, '4n')
+
+    loop.start('1m').stop('10m')
+    loop2.start('4m').stop('10m')
+  },
+  methods: {
+    start: function () {
+      Tone.Transport.start()
+    }
   }
 }
 </script>
